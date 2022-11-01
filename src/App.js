ef73d8747f07220cabe9722ecb7d2a8fa9d97c04
johndoe@example.com <johndoe@example.com>
2022-11-01 23:05:51 +0000

    iteracion3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import contactsArr from "./contacts.json";
+import { userState } from "react";
+
+
+
+function contactRandom() {
+
+  const position = Math.floor(Math.random() * contactsArr.length);
+  const positionAleatori = contactsArr[position]
+  const copyArr = [...contactsArr];
+  copyArr.push(positionAleatori)
+  
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const oscar= '🏆'
+  const fiveContacts = contactsArr.slice(0, 5);
+  return <div className="App">
+    <h2>IronContacts</h2>
+
+    <button onClick={contactRandom}>Random Contact</button>
+    <table>
+
+    <tr>
+    
+    <th>Picture</th>
+    <th>Name</th>
+    <th>Popularity</th>
+    <th>Won Oscar</th>
+    <th>Won Emmy</th>
+
+    </tr>
+
+    {fiveContacts.map((eachelement) =>{
+      return(
+         <tr>
+        <th><img src={eachelement.pictureUrl} /></th>
+        <th><p>{eachelement.name}</p></th>
+        <th><p>{eachelement.popularity.toFixed(2)}</p></th>
+        <th><p>{eachelement.wonOscar ? oscar : "" }</p></th>
+        <th><p>{eachelement.wonEmmy ? oscar : "" }</p></th>
+        </tr>
+      )
+    })}
+
+
+
+
+
+    </table>
+
+    
+  </div>;
 }
+
+
+
 
 export default App;
